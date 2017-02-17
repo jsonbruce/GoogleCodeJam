c3bc5d48bf2e56e76057529cbea6ce7b6780ded0
Max Xu <xuhuan@live.cn>
2017-02-17 17:25:47 +0800

    [Init] 2017APAC-Practice Round A

---
+++ 2017APAC/PracticeRound/LazySpellingBee/LazySpellingBeeSmall.py
@@ -0,0 +1,46 @@
+#/*************************************************************************
+# *  --------->    FILE: LazySpellingBeeSmall.py
+# *  --------->    AUTHOR: Max Xu
+# *  --------->    MAIL: xuhuan@live.cn
+# *  --------->    DATE: 02/17/2017    TIME:09:21:13
+# ************************************************************************/
+#
+
+import os
+
+result = []
+
+with open('A-small-practice.in') as fp:
+    caseCount = fp.readline()
+
+    for i in range(1, int(caseCount)+1):
+        s = fp.readline().strip()
+
+        if len(s) == 1:
+            result.append("Case #" + str(i) + ": " + str(1))
+            continue
+
+        if len(s) == 2:
+            if s[0] == s[1]:
+                result.append("Case #" + str(i) + ": " + str(1))
+            else:
+                result.append("Case #" + str(i) + ": " + str(4))
+            continue
+        
+        ans = 1
+        for j in range(len(s)):
+            if j == 0:
+                ss = set({s[j], s[j+1]})
+            elif j == len(s) - 1:
+                ss = set({s[j], s[j-1]})
+            else:
+                ss = set({s[j-1], s[j], s[j+1]})
+            ans *= len(ss)
+
+
+        result.append("Case #" + str(i) + ": " + str(ans))
+
+with open("A-small-practice.out", "w") as fp:
+    fp.writelines("%s\n" % r for r in result)
+    #for r in result:
+    #    fp.write(r)
---
+++ README.md
@@ -0,0 +1,17 @@
+# Google Code Jam
+
+The website is: https://codejam.withgoogle.com
+
+# Google APAC 2017 University Test
+
+## Practice Round APAC test 2017
+
+## Round A APAC Test 2017
+
+## Round B APAC Test 2017
+
+## Round C APAC Test 2017
+
+## Round D APAC Test 2017
+
+## Round E APAC Test 2017
